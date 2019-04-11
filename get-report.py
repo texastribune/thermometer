b5dad6eb930c9520584fa386549cd4f9fe054105
Daniel Craigmile <dcraigmile@texastribune.org>
2019-04-11 15:10:11 -0500

    change report for SMD19

--- get-report.py
+++ get-report.py
@@ -6,10 +6,10 @@ from s3 import push_to_s3
 
 sf = SalesforceConnection()
 
-path = '/services/data/v35.0/analytics/reports/00O46000000hQLk'
+path = '/services/data/v45.0/analytics/reports/00O0f000007mKFFEA2'
 url = '{}{}'.format(sf.instance_url, path)
 resp = requests.get(url, headers=sf.headers)
 content = json.loads(resp.text)
 the_good_stuff = content['factMap']['T!T']['aggregates'][0]
 the_good_stuff = json.dumps(the_good_stuff)
-push_to_s3(filename='fmd18-members.json', contents=the_good_stuff)
+push_to_s3(filename='smd19.json', contents=the_good_stuff)

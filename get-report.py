182ba6375cd71676acd37b22c18cb0e6821337ae
Daniel Craigmile <dcraigmile@texastribune.org>
2019-06-25 13:59:37 -0500

    change report; upgrade to Python 3

--- get-report.py
+++ get-report.py
@@ -6,10 +6,9 @@ from s3 import push_to_s3
 
 sf = SalesforceConnection()
 
-path = '/services/data/v45.0/analytics/reports/00O0f000007mKFFEA2'
-url = '{}{}'.format(sf.instance_url, path)
+path = "/services/data/v45.0/analytics/reports/00O0f0000089IddEAE"
+url = "{}{}".format(sf.instance_url, path)
 resp = requests.get(url, headers=sf.headers)
 content = json.loads(resp.text)
-the_good_stuff = content['factMap']['T!T']['aggregates'][0]
-the_good_stuff = json.dumps(the_good_stuff)
-push_to_s3(filename='smd19.json', contents=the_good_stuff)
+the_good_stuff = content["factMap"]["T!T"]["aggregates"][0]
+push_to_s3(filename="border2019.json", contents=the_good_stuff)
--- s3.py
+++ s3.py
@@ -1,26 +1,33 @@
-from datetime import datetime, timedelta
-import gzip
-import StringIO
+# from datetime import datetime, timedelta
+# import gzip
+# import StringIO
 
-from boto.s3.connection import S3Connection, OrdinaryCallingFormat
-from boto.s3.key import Key
+# from boto.s3.connection import S3Connection, OrdinaryCallingFormat
+# from boto.s3.key import Key
+
+import boto3
+import json
 
 
-def push_to_s3(filename=None, contents=None):
+def push_to_s3(filename, contents):
+    # Parameterized for testing.
+    # Should this be a setting rather than a hardcoded string?
+    s3_bucket = "membership.texastribune.org"
+    breakpoint()
+    # Create an S3 client, gives you lowest level access to S3 control
+    s3_client = boto3.client(
+        "s3",
+        #        aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
+        #        aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY,
+    )
 
-    out = StringIO.StringIO()
-    with gzip.GzipFile(fileobj=out, mode="w") as f:
-        f.write(contents)
-
-    conn = S3Connection(calling_format=OrdinaryCallingFormat())
-    bucket = conn.get_bucket('membership.texastribune.org')
-    k = Key(bucket)
-    k.key = filename
-    k.set_contents_from_string(out.getvalue(),
-            policy='public-read',
-            headers={
-                'Cache-Control': 'no-cache',
-                'Content-Type': 'application/json',
-                'Content-Encoding': 'gzip',
-                })
-    k.set_acl('public-read')
+    # Write the JSON-formatted SF data to the S3 bucket
+    # Future add: Set cache-control in header
+    #             Set content-type
+    #             Set compression
+    s3_client.put_object(
+        ACL="public-read",
+        Body=json.dumps(contents, indent=2),
+        Bucket=s3_bucket,
+        Key=filename,
+    )

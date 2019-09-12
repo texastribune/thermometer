e175b94848fb81a3a555e9380d768b98acf5d62c
Daniel Craigmile <dcraigmile@texastribune.org>
2019-09-12 15:03:23 -0500

    update for FMD 2019

--- get-report.py
+++ get-report.py
@@ -6,9 +6,10 @@ from s3 import push_to_s3
 
 sf = SalesforceConnection()
 
-path = "/services/data/v45.0/analytics/reports/00O0f0000089IddEAE"
+path = "/services/data/v46.0/analytics/reports/00O0f000007K5uiEAC"
 url = "{}{}".format(sf.instance_url, path)
 resp = requests.get(url, headers=sf.headers)
 content = json.loads(resp.text)
 the_good_stuff = content["factMap"]["T!T"]["aggregates"][0]
-push_to_s3(filename="border2019.json", contents=the_good_stuff)
+print(the_good_stuff)
+push_to_s3(filename="fmd2019.json", contents=the_good_stuff)

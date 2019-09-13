42f396cf02f482241f14d4ff72832cda8b3dc79c
Daniel Craigmile <dcraigmile@texastribune.org>
2019-09-13 14:42:09 -0500

    number of rows is in a different place

--- get-report.py
+++ get-report.py
@@ -10,6 +10,7 @@ path = "/services/data/v46.0/analytics/reports/00O0f000007K5uiEAC"
 url = "{}{}".format(sf.instance_url, path)
 resp = requests.get(url, headers=sf.headers)
 content = json.loads(resp.text)
-the_good_stuff = content["factMap"]["T!T"]["aggregates"][0]
+print(content)
+the_good_stuff = content["factMap"]["T!T"]["aggregates"][3]
 print(the_good_stuff)
 push_to_s3(filename="fmd2019.json", contents=the_good_stuff)

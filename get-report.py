a1b58c6dd97a78bcdfd6c4ce6c07f28b68d87777
AndrewGibson27 <atgibson27@gmail.com>
2020-08-27 12:09:21 -0600

    new report for FMD2020

--- get-report.py
+++ get-report.py
@@ -6,11 +6,11 @@ from s3 import push_to_s3
 
 sf = SalesforceConnection()
 
-path = "/services/data/v46.0/analytics/reports/00O0f000007K5uiEAC"
+path = "/services/data/v46.0/analytics/reports/00O6f000007rrXfEAI"
 url = "{}{}".format(sf.instance_url, path)
 resp = requests.get(url, headers=sf.headers)
 content = json.loads(resp.text)
 print(content)
 the_good_stuff = content["factMap"]["T!T"]["aggregates"][3]
 print(the_good_stuff)
-push_to_s3(filename="fmd2019.json", contents=the_good_stuff)
+push_to_s3(filename="fmd2020.json", contents=the_good_stuff)

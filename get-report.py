8e50e9acde135c37d340df966dc88e020bd3488c
matthewdylan <88053677+matthewdylan@users.noreply.github.com>
2022-04-25 10:28:49 -0500

    updating report and file names
    for spring membership drive 2022 prep

--- get-report.py
+++ get-report.py
@@ -6,11 +6,11 @@ from s3 import push_to_s3
 
 sf = SalesforceConnection()
 
-path = "/services/data/v46.0/analytics/reports/00O6f00000874N2EAI"
+path = "/services/data/v46.0/analytics/reports/00O6f0000087qHcEAI"
 url = "{}{}".format(sf.instance_url, path)
 resp = requests.get(url, headers=sf.headers)
 content = json.loads(resp.text)
 print(content)
 the_good_stuff = content["factMap"]["T!T"]["aggregates"][3]
 print(the_good_stuff)
-push_to_s3(filename="fmd2021.json", contents=the_good_stuff)
+push_to_s3(filename="smd2022.json", contents=the_good_stuff)

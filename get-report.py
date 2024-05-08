f9b56607b1cfc08d75f431945b7ffe548e6a359a
Jonathan Peacher <djpeacher@gmail.com>
2024-05-08 12:14:41 -0500

    fix(smd): set array_index = 3

--- get-report.py
+++ get-report.py
@@ -7,7 +7,7 @@ from s3 import push_to_s3
 sf = SalesforceConnection()
 
 # set array_index to 0 for Giving Tuesday report and 3 for SMD/FMD report
-array_index = 0
+array_index = 3
 path = "/services/data/v46.0/analytics/reports/00OPe000007npIKMAY"
 filename = "smd2024.json"
 

d38230b41e78a92e0f3f170d17ae4c5bfa2569b1
Ashley Hebler <ashleyhebler@gmail.com>
2024-12-23 11:34:52 -0600

    fix(eoy): try other option
    To see if this gets the job to succeed

--- get-report.py
+++ get-report.py
@@ -7,7 +7,7 @@ from s3 import push_to_s3
 sf = SalesforceConnection()
 
 # set array_index to 0 for Giving Tuesday report and 3 for SMD/FMD report
-array_index = 3
+array_index = 0
 path = "/services/data/v46.0/analytics/reports/00OPe00000GWBK5MAP"
 filename = "eoy2024.json"
 

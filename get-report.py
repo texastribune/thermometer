49e0b373788a0d5d1f76a31d47012b1afdf45fc3
matthewdylan <88053677+matthewdylan@users.noreply.github.com>
2025-05-07 20:35:54 -0500

    update for smd2025
    yea

--- get-report.py
+++ get-report.py
@@ -6,10 +6,10 @@ from s3 import push_to_s3
 
 sf = SalesforceConnection()
 
-# set array_index to 0 for Giving Tuesday report and 3 for SMD/FMD report
-array_index = 2
-path = "/services/data/v46.0/analytics/reports/00OPe00000GWBK5MAP"
-filename = "eoy2024.json"
+# set array_index to 0 for Giving Tuesday report, 2 for EOY report and 3 for SMD/FMD report
+array_index = 3
+path = "/services/data/v46.0/analytics/reports/00OPe00000MIHAgMAP"
+filename = "smd2025.json"
 
 url = "{}{}".format(sf.instance_url, path)
 resp = requests.get(url, headers=sf.headers)

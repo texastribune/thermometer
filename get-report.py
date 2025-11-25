2e0cb10e7281664c4ddc3cb32431e09823f55390
matthewdylan <88053677+matthewdylan@users.noreply.github.com>
2025-11-25 09:15:35 -0600

    change index
    hope this one is right

--- get-report.py
+++ get-report.py
@@ -7,7 +7,7 @@ from s3 import push_to_s3
 sf = SalesforceConnection()
 
 # set array_index to 0 for Giving Tuesday report, 2 for EOY report and 3 for SMD/FMD report
-array_index = 3
+array_index = 0
 path = "/services/data/v46.0/analytics/reports/00OPe00000ZUkIzMAL"
 filename = "gt2025.json"
 

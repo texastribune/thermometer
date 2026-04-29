dd61b0aefe6f4326ffccc33b754bbc34465254e6
matthewdylan <88053677+matthewdylan@users.noreply.github.com>
2026-04-29 18:06:51 -0500

    add report for smd 26
    assuming index 3

--- get-report.py
+++ get-report.py
@@ -7,9 +7,9 @@ from s3 import push_to_s3
 sf = SalesforceConnection()
 
 # set array_index to 0 for Giving Tuesday report, 2 for EOY report and 3 for SMD/FMD report
-array_index = 0
-path = "/services/data/v46.0/analytics/reports/00OPe00000bhsavMAA"
-filename = "eoy2025.json"
+array_index = 3
+path = "/services/data/v46.0/analytics/reports/00OPe00000kb4RxMAI"
+filename = "smd2026.json"
 
 url = "{}{}".format(sf.instance_url, path)
 resp = requests.get(url, headers=sf.headers)
